10bb123fcd50b466e392f8d0228c389963cc8afa
dudaeitelven <32472327+dudaeitelven@users.noreply.github.com>
2021-06-27 21:02:51 -0300

    Ajustes

--- sniffer.py
+++ sniffer.py
@@ -1,6 +1,5 @@
 import socket
 import struct
-import textwrap
 import binascii
 import struct
 import sys
@@ -18,7 +17,6 @@ def main():
         print('Socket could not be created.')
         sys.exit(1)
     
-
     while True:
         raw_data, addr = conn.recvfrom(65536)
         ethernet_protocol = ""
@@ -28,25 +26,41 @@ def main():
         protoType = IpHeader[2] 
         next_protocol = hex(protoType) 
 
-        #Check the protocol type
         if (next_protocol == '0x800'): 
             ethernet_protocol = 'IPV4'
         elif (next_protocol == '0x86dd'): 
             ethernet_protocol = 'IPV6'
 
         data = raw_data[14:]
-
         
         if (ethernet_protocol == 'IPV6'):
-            new_packet, next_protocol = ipv6Header(data)
+            ipv6_first_word, ipv6_payload_legth, next_protocol, ipv6_hoplimit = struct.unpack(">IHBB", data[0:8])
+            ipv6_src_ip = socket.inet_ntop(socket.AF_INET6, data[8:24])
+            ipv6_dst_ip = socket.inet_ntop(socket.AF_INET6, data[24:40])
+
+            bin(ipv6_first_word)
+            "{0:b}".format(ipv6_first_word)
+            version = ipv6_first_word >> 28
+            traffic_class = ipv6_first_word >> 16
+            traffic_class = int(traffic_class) & 4095
+            flow_label = int(ipv6_first_word) & 65535
+
+            if (next_protocol == 6):
+                next_protocol = 'TCP'
+            elif (next_protocol == 17):
+                next_protocol = 'UDP'
+            elif (next_protocol == 58):
+                next_protocol = 'ICMPv6'
+
+            new_packet = data[40:]
 
             if ((next_protocol == 'ICMPv6') and (filtro == 'ICMP')):
                 type, code, chekcsum = struct.unpack(">BBH", new_packet[:4])
 
                 print('  --  ICMP v6  --  ')
-                print('\tType: %s' % type)
-                print('\tCode: %s' % code)
-                print('\tChecksum: %s' % chekcsum)
+                print('Type: %s' % type)
+                print('Code: %s' % code)
+                print('Checksum: %s' % chekcsum)
                 print(' ')
 
             elif ((next_protocol == 'TCP') and (filtro == 'TCP')):
@@ -63,16 +77,16 @@ def main():
                 urgPntr = packet[7]
 
                 print('  --  TCP v6 --  ')
-                print('\tSource Port: %s' % srcPort)
-                print('\tDestination Port: %s' % dstPort)
-                print('\tSequence Number: %s' % sqncNum)
-                print('\tAck. Number: %s' % acknNum)
-                print('\tData Offset: %s' % dataOffset)
-                print('\tReserved: %s' % reserved)
-                print('\tTCP Flags: %s' % tcpFlags) 
-                print('\tWindow: %s' % window)
-                print('\tChecksum: %s' % checkSum)
-                print('\tUrgent Pointer: %s' % urgPntr)
+                print('Source Port: %s' % srcPort)
+                print('Destination Port: %s' % dstPort)
+                print('Sequence Number: %s' % sqncNum)
+                print('Ack. Number: %s' % acknNum)
+                print('Data Offset: %s' % dataOffset)
+                print('Reserved: %s' % reserved)
+                print('TCP Flags: %s' % tcpFlags) 
+                print('Window: %s' % window)
+                print('Checksum: %s' % checkSum)
+                print('Urgent Pointer: %s' % urgPntr)
                 print(' ')
                 
             elif ((next_protocol == 'UDP') and (filtro == 'UDP')):
@@ -83,14 +97,13 @@ def main():
                 checkSum = packet[3]
 
                 print('  --  UDP v6 --  ')
-                print('\tSource Port: %s' % srcPort)
-                print('\tDestination Port: %s' % dstPort)
-                print('\tLenght: %s' % lenght)
-                print('\tChecksum: %s' % checkSum)
+                print('Source Port: %s' % srcPort)
+                print('Destination Port: %s' % dstPort)
+                print('Lenght: %s' % lenght)
+                print('Checksum: %s' % checkSum)
                 print(' ')
 
-        elif (ethernet_protocol == 'IPV4'):
-            
+        elif (ethernet_protocol == 'IPV4'):    
             version_header_len = data[0]
             version = version_header_len >> 4
             header_length = (version_header_len & 15) * 4
@@ -99,104 +112,61 @@ def main():
             target = '.'.join(map(str, target))            
             data = data[header_length:]
 
-
             if ((protocol == 1) and (filtro == 'ICMP')):
                 icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
                 data = data[4:]
  
                 print("  --  ICMP v4 --  ")
-                print("\tICMP type: %s" % icmp_type)
-                print("\tICMP code: %s" % code)
-                print("\tICMP checksum: %s" % checksum)
+                print("ICMP type: %s" % icmp_type)
+                print("ICMP code: %s" % code)
+                print("ICMP checksum: %s" % checksum)
                 print(' ')
 
             elif ((protocol == 6) and (filtro == 'TCP')):
                 print("  --  TCP v4  --  ")
-                print('\tVersion: %s' % version)
-                print('\tHeader Length: %s' % header_length)
-                print('\tTTL: %s' % ttl)
-                print('\tProtocol: %s' % protocol)
-                print('\tSource: %s' % source)
-                print('\tTarget: %s' % target)
+                print('Version: %s' % version)
+                print('Header Length: %s' % header_length)
+                print('TTL: %s' % ttl)
+                print('Protocol: %s' % protocol)
+                print('Source: %s' % source)
+                print('Target: %s' % target)
                 print('')
 
                 source_port, destination_port, sequence, acknowledgment, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin = struct.unpack(
                     '! H H L L H H H H H H', raw_data[:24])
                 
                 print('\t  ---  TCP Segment  ---  ')
-                print('\t\tSource Port: %s' % source_port)
-                print('\t\tDestination Port: %s' % destination_port)
-                print('\t\tSequence: %s' % sequence)
-                print('\t\tAcknowledgment: %s' % acknowledgment)
+                print('\tSource Port: %s' % source_port)
+                print('\tDestination Port: %s' % destination_port)
+                print('\tSequence: %s' % sequence)
+                print('\tAcknowledgment: %s' % acknowledgment)
                 print('')
 
                 print('\t  ---  Flags  ---  ')
-                print('\t\tURG: %s' % flag_urg)
-                print('\t\tACK: %s' % flag_ack)
-                print('\t\tPSH: %s' % flag_psh)
-                print('\t\tRST: %s' % flag_rst)
-                print('\t\tSYN: %s' % flag_syn)
-                print('\t\tFIN: %s' % flag_fin)
+                print('\tURG: %s' % flag_urg)
+                print('\tACK: %s' % flag_ack)
+                print('\tPSH: %s' % flag_psh)
+                print('\tRST: %s' % flag_rst)
+                print('\tSYN: %s' % flag_syn)
+                print('\tFIN: %s' % flag_fin)
                 print('')
 
             elif ((protocol == 17) and (filtro == 'UDP')):
                 print("  --  UDP v4  --  ")
-                print('\tVersion: %s' % version)
-                print('\tHeader Length: %s' % header_length)
-                print('\tTTL: %s' % ttl)
-                print('\tProtocol: %s' % protocol)
-                print('\tSource: %s' % source)
-                print('\tTarget: %s' % target)
+                print('Version: %s' % version)
+                print('Header Length: %s' % header_length)
+                print('TTL: %s' % ttl)
+                print('Protocol: %s' % protocol)
+                print('Source: %s' % source)
+                print('Target: %s' % target)
                 print('')
 
                 source_port, destination_port, length = struct.unpack('! H H 2x H', data[:8])
                 data = data[8:]
                 print('\t  ---  UDP Segment  ---  ')
-                print('\t\tSource Port: %s' % source_port)
-                print('\t\tDestination Port: %s' % destination_port)
-                print('\t\tLength: %s' % length)
+                print('\tSource Port: %s' % source_port)
+                print('\tDestination Port: %s' % destination_port)
+                print('\tLength: %s' % length)
                 print('')
-
-
-def nextHeader(ipv6_next_header):
-    if (ipv6_next_header == 6):
-        ipv6_next_header = 'TCP'
-    elif (ipv6_next_header == 17):
-        ipv6_next_header = 'UDP'
-    elif (ipv6_next_header == 43):
-        ipv6_next_header = 'Routing'
-    elif (ipv6_next_header == 1):
-        ipv6_next_header = 'ICMP'
-    elif (ipv6_next_header == 58):
-        ipv6_next_header = 'ICMPv6'
-    elif (ipv6_next_header == 44):
-        ipv6_next_header = 'Fragment'
-    elif (ipv6_next_header == 0):
-        ipv6_next_header = 'HOPOPT'
-    elif (ipv6_next_header == 60):
-        ipv6_next_header = 'Destination'
-    elif (ipv6_next_header == 51):
-        ipv6_next_header = 'Authentication'
-    elif (ipv6_next_header == 50):
-        ipv6_next_header = 'Encapsuling'
-
-    return ipv6_next_header
-
-def ipv6Header(data):
-    ipv6_first_word, ipv6_payload_legth, ipv6_next_header, ipv6_hoplimit = struct.unpack(">IHBB", data[0:8])
-    ipv6_src_ip = socket.inet_ntop(socket.AF_INET6, data[8:24])
-    ipv6_dst_ip = socket.inet_ntop(socket.AF_INET6, data[24:40])
-
-    bin(ipv6_first_word)
-    "{0:b}".format(ipv6_first_word)
-    version = ipv6_first_word >> 28
-    traffic_class = ipv6_first_word >> 16
-    traffic_class = int(traffic_class) & 4095
-    flow_label = int(ipv6_first_word) & 65535
-
-    ipv6_next_header = nextHeader(ipv6_next_header)
-    data = data[40:]
-
-    return data, ipv6_next_header
 
 main()
